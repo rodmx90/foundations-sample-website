762f679c1986f660383dd0ec5dc8d80469dba0e7
Rodrigo Orozco <rod.mx90@gmail.com>
2021-03-11 19:37:57 +0100

    color-names json file path update

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -7,11 +7,12 @@
 # letting them know that their color doesn't exist.
 import json
 
+
 def get_color_code(color_name):
     # this is where you should add your logic to check the color.
     # Open the file at data/css-color-names.json, and return the hex code
     # The file can be considered as JSON format, or as a Python dictionary.        
-    with open('data/css-color-names.json') as colors:
+    with open('color_check/data/css-color-names.json') as colors:
         colorlist = json.load(colors)        
 
         if color_name in colorlist:
--- color_check/website.py
+++ color_check/website.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask import render_template
 from color_check.controllers.get_color_code import get_color_code
+
 app = Flask(__name__)
 
 
